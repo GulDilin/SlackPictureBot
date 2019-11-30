5fb85afbadfc12a442f3047dc1dea983bc564b23
GulDilin <zhenyagurin@gmail.com>
2019-11-30 12:55:30 +0300

    "try to deploy on heroku"

--- picturebot/myapp.py
+++ picturebot/myapp.py
@@ -9,11 +9,12 @@ app = Flask(__name__)
 @app.route('/webhook')
 def hello_slack():
     # получили данные из запроса
-    request_json = request.get_json(silent=True, force=True)
+    resp = request.data
+    # request_json = request.get_json(silent=True, force=True)
     # тут ваш код возьмет запрос и вернет в ответ любой dict объект ответа, можно даже пустой
     # примерно так request_json -> response_body_json
-    print(request_json)
-    response_body = json.dumps(request_json)
+    print(resp)
+    response_body = json.dumps(resp)
     # упаковали все в корректный респонс
     response = make_response(response_body)
     response.headers['Content-Type'] = 'application/json'
